f662b3d275bca3f59a73e5c45ae87b86df9bd6b4
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2022-06-17 23:55:00 -0700

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='meme-python',
-    version='1.0.1',
+    version='1.0.2',
     license='MIT',
     author='Gabe Millikan',
     url='https://github.com/GabeMillikan/meme-python',
